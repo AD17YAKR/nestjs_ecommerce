8f56e0b8a8561b69d5531cc9e1ebb1979af78622
AD17YAKR <adityasingh.110601@gmail.com>
2022-12-19 02:50:27 +0530

    ADD: database connection and basic changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  //TODO: change url to env variable
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/e_commerce')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const url = process.env.MONGO_URL;
+  console.log(url);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
